0d1645b7f88f00e7c67213832118440c88b89806
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2022-07-27 12:03:20 +0200

    First working version

---
+++ app.js
@@ -0,0 +1,38 @@
+const img = document.getElementById('img')
+const pos = document.getElementById('pos')
+
+const fileInput = document.getElementById('file')
+function updateFile () {
+  const [file] = fileInput.files
+  if (file) {
+    img.src = URL.createObjectURL(file)
+  }
+}
+
+fileInput.onchange = updateFile
+updateFile()
+
+function get_pos (e) {
+  const x = e.clientX - img.offsetLeft
+  const y = e.clientY - img.offsetTop
+
+  const px = (100 * x / img.offsetWidth).toFixed(2)
+  const py = (100 * y / img.offsetHeight).toFixed(2)
+
+  return [px, py]
+}
+
+img.ondragstart = (e) => {
+  return false
+}
+
+img.onmousedown = (e) => {
+  const p = get_pos(e)
+  pos.innerHTML = p[0] + ',' + p[1] + ','
+}
+
+img.onmouseup = (e) => {
+  const p = get_pos(e)
+  pos.innerHTML += p[0] + ',' + p[1]
+  return false
+}
